40a9e4fb43d9727fcd2258d6f1ca0109b9ac55bb
Aidan Timson <contact@timmo.xyz>
2021-07-31 17:25:26 +0100

    Add dotNet application

---
+++ SystemBridgeWindowsSensors/Program.cs
@@ -0,0 +1,88 @@
+﻿using System;
+using LibreHardwareMonitor.Hardware;
+using Newtonsoft.Json;
+using Newtonsoft.Json.Linq;
+
+namespace SystemBridgeWindowsSensors
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Computer computer = new Computer
+            {
+                IsCpuEnabled = true,
+                IsGpuEnabled = true,
+                IsMemoryEnabled = true,
+                IsMotherboardEnabled = true,
+                IsControllerEnabled = true,
+                IsNetworkEnabled = true,
+                IsStorageEnabled = true
+            };
+
+            computer.Open();
+            computer.Accept(new UpdateVisitor());
+
+
+            JArray arrRoot = new JArray();
+
+            foreach (IHardware hardware in computer.Hardware)
+            {
+                JObject objHardware = new JObject
+                {
+                    ["id"] = hardware.Identifier.ToString(),
+                    ["name"] = hardware.Name,
+                    ["type"] = hardware.HardwareType.ToString(),
+                };
+
+                JArray arrSubHardware = new JArray();
+
+                foreach (IHardware subhardware in hardware.SubHardware)
+                {
+                    JObject objSubHardware = new JObject
+                    {
+                        ["id"] = hardware.Identifier.ToString(),
+                        ["name"] = hardware.Name,
+                        ["type"] = hardware.HardwareType.ToString(),
+                    };
+
+                    JArray arrSubHardwareSensors = new JArray();
+
+                    foreach (ISensor sensor in subhardware.Sensors)
+                    {
+                        arrSubHardwareSensors.Add(new JObject
+                        {
+                            ["id"] = sensor.Identifier.ToString(),
+                            ["name"] = sensor.Name,
+                            ["type"] = sensor.SensorType.ToString(),
+                            ["value"] = sensor.Value,
+                        });
+                    }
+                    arrSubHardwareSensors["sensors"] = arrSubHardwareSensors;
+                    arrSubHardware.Add(arrSubHardwareSensors);
+                }
+
+                objHardware["subhardware"] = arrSubHardware;
+
+                JArray arrSensors = new JArray();
+
+                foreach (ISensor sensor in hardware.Sensors)
+                {
+                    arrSensors.Add(new JObject
+                    {
+                        ["id"] = sensor.Identifier.ToString(),
+                        ["name"] = sensor.Name,
+                        ["type"] = sensor.SensorType.ToString(),
+                        ["value"] = sensor.Value,
+                    });
+                }
+
+                objHardware["sensors"] = arrSensors;
+
+                arrRoot.Add(objHardware);
+            }
+
+            Console.WriteLine(arrRoot.ToString(Formatting.None));
+        }
+    }
+}
