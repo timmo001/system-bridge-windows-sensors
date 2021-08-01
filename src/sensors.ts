f1e5d26e4b787b56fd77bd92b91a5a5099aaf00a
Aidan Timson <contact@timmo.xyz>
2021-08-01 13:48:16 +0100

    Change directory of output

--- src/sensors.ts
+++ src/sensors.ts
@@ -3,6 +3,12 @@ import execa from "execa";
 
 import { Hardware, Sensor } from "./types";
 
+const PATH = join(
+  __dirname,
+  "../WindowsSensors",
+  "SystemBridgeWindowsSensors.exe"
+);
+
 function cleanupData(data: Array<Hardware>): Array<Hardware> {
   return data.map((hardware: Hardware) => {
     hardware.sensors = hardware.sensors.map((sensor: Sensor) => {
@@ -13,41 +19,23 @@ function cleanupData(data: Array<Hardware>): Array<Hardware> {
   });
 }
 
-export async function getAllHardware(
-  debug?: boolean
-): Promise<Array<Hardware>> {
-  const { stdout, stderr } = await execa(
-    join(
-      __dirname,
-      "../SystemBridgeWindowsSensors/bin",
-      debug ? "Debug" : "Release",
-      "SystemBridgeWindowsSensors.exe"
-    )
-  );
+export async function getAllHardware(): Promise<Array<Hardware>> {
+  const { stdout, stderr } = await execa(PATH);
   if (stderr) throw new Error(stderr);
   return cleanupData(JSON.parse(stdout));
 }
 
-export async function getHardwareById(
-  id: string,
-  debug?: boolean
-): Promise<Hardware> {
-  const hardwareData = await getAllHardware(debug);
+export async function getHardwareById(id: string): Promise<Hardware> {
+  const hardwareData = await getAllHardware();
   return hardwareData.find((hardware: Hardware) => hardware.id === id);
 }
 
-export async function getHardwareByName(
-  name: string,
-  debug?: boolean
-): Promise<Hardware> {
-  const hardwareData = await getAllHardware(debug);
+export async function getHardwareByName(name: string): Promise<Hardware> {
+  const hardwareData = await getAllHardware();
   return hardwareData.find((hardware: Hardware) => hardware.name === name);
 }
 
-export async function getHardwareByType(
-  type: string,
-  debug?: boolean
-): Promise<Hardware> {
-  const hardwareData = await getAllHardware(debug);
+export async function getHardwareByType(type: string): Promise<Hardware> {
+  const hardwareData = await getAllHardware();
   return hardwareData.find((hardware: Hardware) => hardware.type === type);
 }
